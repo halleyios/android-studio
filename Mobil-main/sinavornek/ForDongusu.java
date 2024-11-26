73e7b03e817bfc7cc3380c858a97fce287d5d3a4
halley <130781977+halleyios@users.noreply.github.com>
2024-11-26 11:33:23 +0300

    Delete Mobil-main directory

--- Mobil-main/sinavornek/ForDongusu.java
+++
@@ -1,9 +0,0 @@
-package com.example.p20241008;
-
-public class ForDongusu {
-    public static void main(String[] args) {
-        for (int i=1;i<=10;i++){
-            System.out.println(i+"Hikmet Nazif");
-        }
-    }
-}
--- Mobil-main/uygulama1/Ornek2.java
+++
@@ -1,8 +0,0 @@
-package com.example.uygulama1;
-
-public class Ornek2 {
-
-    public static void main(String[] args) {
-        System.out.println("Merhaba Java!!!");
-    }
-}
